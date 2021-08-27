03c74e6752342359e2ae404a1c35b672dfb65a02
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-27 22:43:32 +0530

    Add files via upload

---
+++ Service/TaskService.cs
@@ -0,0 +1,38 @@
+﻿using PROTOFOLIO.Models;
+using PROTOFOLIO.Repositary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROTOFOLIO.Service
+{
+    public class TaskService
+    {
+        private TaskRepositary taskRepositary;
+
+        public TaskService()
+        {
+            taskRepositary = new TaskRepositaryimpl();
+        }
+        public Task CreateTask(Task task)
+        {
+            return taskRepositary.CreateTask(task);
+        }
+        public Task FindTaskById(int id)
+        {
+            return taskRepositary.GetTaskById(id);
+        }
+        public List<Task> GetAllTask()
+        {
+            return taskRepositary.GetAllTasks();
+        }
+
+        public string removeTask(int id)
+        {
+            return taskRepositary.DeleteTask(id);
+        }
+
+
+    }
+}
